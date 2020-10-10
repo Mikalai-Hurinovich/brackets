5fcae599ad74edbbf544029380b5555a13a59b97
Mikalai Hurynovich <gurinovich-ns@mail.ru>
2020-10-10 11:21:22 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    if (str.length % 2 !== 0) {
+        return false;
+    }
+    let arr = new Array();
+
+    for (let i = 0; i < str.length; i++) {
+            let result = false;
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            if (str[i] == bracketsConfig[j][0]) {
+                result = true;
+                if (bracketsConfig[j][0] == bracketsConfig[j][1]) {
+                    if (arr[arr.length - 1] == bracketsConfig[j][0]) {
+                        arr.pop();
+                        break;
+                    }
+                }
+                arr.push(bracketsConfig[j][1]);
+                break;
+            }
+        }
+        if (!result) {
+            if (arr.length == 0 || str[i] != arr[arr.length - 1]) {
+                return false;
+            }
+            arr.pop();
+        }
+    }
+    return arr.length == 0 ? true : false;
 }
